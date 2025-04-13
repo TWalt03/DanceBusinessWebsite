48335394baa8841e17194e07d55fe20c5731d14f
TWalt03 <tylerwalters537@gmail.com>
2025-04-13 20:42:18 +0200

    Home Pictures

--- sidebar.js
+++ sidebar.js
@@ -1,13 +1,19 @@
-const sidebar = document.querySelector('.sidebar')
-const toggleButton = document.querySelector('.menubtn')
+const sidebar = document.querySelector(".sidebar");
+const toggleButton = document.querySelector(".menubtn");
+const overlay = document.querySelector(".overlay");
 
-
-toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('active')
+toggleButton.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
-document.addEventListener('keydown', (event) => {
-    if(event.key === 'Escape'){
-        sidebar.classList.remove('active');
-    }
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+});
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
 });
